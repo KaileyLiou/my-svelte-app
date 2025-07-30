d3941ea1299e6ce5275b1f803c1794bc4ab21a66
KaileyLiou <kaileyliou09@gmail.com>
2025-07-30 14:16:04 -0400

    added code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-svelte-app',
-  plugins: [svelte()],
-})
+  base: '/my-svelte-app/',
+  plugins: [svelte({ preprocess: vitePreprocess() })]
+});
